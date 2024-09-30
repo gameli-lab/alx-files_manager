a8e09a7451cb652d9dfb09cff0d4ba8d343b4c70
Benjamin Torfu <btorfu@proton.me>
2024-09-30 23:48:14 +0000

    server

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const AppController = require('../controllers/AppController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', AppController.postNew);
+router.get('/connect', AppController.getConnect);
+router.get('/disconnect', AppController.getDisconnect);
+router.get('/users/me', AppController.getMe);
 
 module.exports = router;
