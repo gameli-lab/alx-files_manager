18cb9b31838cc353a56112290473460c9843c419
Benjamin Torfu <btorfu@proton.me>
2024-09-30 23:58:41 +0000

    server

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,12 @@ router.post('/users', AppController.postNew);
 router.get('/connect', AppController.getConnect);
 router.get('/disconnect', AppController.getDisconnect);
 router.get('/users/me', AppController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
 
 module.exports = router;
