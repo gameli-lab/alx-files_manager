8655f98e6846f07e2d77cdec8519e47701ff1ad8
Benjamin Torfu <btorfu@proton.me>
2024-10-01 00:02:58 +0000

    server

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', AppController.postNew);
-router.get('/connect', AppController.getConnect);
-router.get('/disconnect', AppController.getDisconnect);
-router.get('/users/me', AppController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/connect', UsersController.getConnect);
+router.get('/disconnect', UsersController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
